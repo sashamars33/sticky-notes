0a4e0b35992d994449e1b184f48a3aa3815bb2c7
Sasha Marshall <sashamarshalldesigns@gmail.com>
2023-03-23 19:53:55 -0700

    debugging production

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,42 @@ const app = express()
 //Database Connection
 connectDB()
 
+console.log('connected')
+
 app.use(cors())
+
+console.log('cors')
+
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
+console.log('json, express')
+
 
 //Routes
 app.use('/api/users', require('./routes/userRoutes'))
 app.use('/api/pages', require('./routes/pageRoutes'))
 
+console.log('routes')
+
+
 //Set build as Static Folder
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 
+console.log('static')
+
+
 app.get('*', (req,res) => res.sendFile(__dirname, '../', 'frontend', 'build', 'index.html'))
+
+console.log('file send')
 
 
 //Error Handler
 app.use(errorHandler)
+
+console.log('error handling')
+ 
 
 app.listen(PORT, () => {
     if(process.env.ENV === "dev"){
