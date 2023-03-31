e1756573722e0921d3198f3d710b0115cc7d5f40
Sasha Marshall <sashamarshalldesigns@gmail.com>
2023-03-30 18:56:06 -0700

    updates to webpack

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -11,5 +11,12 @@ module.exports = {
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
+    new CompressionPlugin({
+      asset: "[path].gz[query]",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8
+    })
   ],
 };
